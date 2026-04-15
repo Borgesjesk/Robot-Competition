28d515f7014e1cc336c6bcee6337793714b1c5c8
BORGES <jessborgesb@gmail.com>
2026-04-15 11:23:22 +0200

    feat: add Robot Registration Service

---
+++ src/main/java/Level_Exam/service/RobotRegistration.java
@@ -0,0 +1,44 @@
+package Level_Exam.service;
+
+import Level_Exam.domain.LandRobot;
+import Level_Exam.domain.ResistanceEvaluable;
+import Level_Exam.domain.Robot;
+
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class RobotRegistration {
+
+    private List<Robot> robots = new ArrayList<>();
+
+
+    public void addRobot(Robot robot) {
+        Objects.requireNonNull(robot, "Robot cannot be null");
+        robots.add(robot);
+    }
+
+    public void listRobots() {
+        robots.forEach(robot -> System.out.println(robot.getTechnicalDescription()));
+    }
+
+    public List<Robot> filterByMaxSpeed(int maxSpeed) {
+        return robots.stream()
+                .filter(r -> r instanceof LandRobot landRobot && landRobot.getMaxSpeed() > maxSpeed)
+                .toList();
+    }
+
+    public List<Robot> filterByManufacturer(String manufacturer) {
+        return robots.stream()
+                .filter(r -> r.getManufacturer().equals(manufacturer))
+                .toList();
+    }
+
+    public List<ResistanceEvaluable> getEvaluableRobots() {
+        return robots.stream()
+                .filter(r -> r instanceof ResistanceEvaluable)
+                .map(r -> (ResistanceEvaluable) r)
+                .toList();
+    }
+}
