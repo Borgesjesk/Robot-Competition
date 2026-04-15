23c836725a9a6c0ab7f540ad462043f95ab61566
BORGES <jessborgesb@gmail.com>
2026-04-15 11:34:26 +0200

    feat: add Main app

---
+++ src/main/java/Level_Exam/app/Main.java
@@ -0,0 +1,31 @@
+package Level_Exam.app;
+
+import Level_Exam.domain.*;
+import Level_Exam.service.RobotRegistration;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.List;
+
+import static java.util.Locale.filter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        RobotRegistration robot = new RobotRegistration();
+
+        robot.addRobot(new LandRobot("Optimus", "TESLA", Year.of(2026), LocalDate.now(), 60, TractionType.TRACK));
+        robot.addRobot(new AirRobot("A2", "Agibot", Year.of(2025), LocalDate.now(), 200, 120));
+        robot.addRobot(new WaterRobot("BlueROV2", "BlueRobotics", Year.of(2024), LocalDate.now(), 150, PropulsionType.JET));
+        robot.addRobot(new LandRobot("T71", "TESLA", Year.of(2026), LocalDate.now(), 55, TractionType.WHEEL));
+
+        System.out.print(" === List of Robots === ");
+        robot.listRobots();
+
+        System.out.print(" === List of Land Robots with Speed greater than 50km/hr === ");
+        robot.filterByMaxSpeed(50).forEach(r -> System.out.print(r.getName() + " | " + r.getManufacturer()));
+
+        System.out.println(" === List of Robots by Manufacturer === ");
+        robot.filterByManufacturer("TESLA").forEach(r -> System.out.print(r.getName() + " | " + r.getManufacturer()));
+    }
+}
